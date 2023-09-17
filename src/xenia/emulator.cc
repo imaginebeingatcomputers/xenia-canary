89df902922f64267f7d33380aee63b9c6ffadd83
imaginebeingatcomputers <ibac@lxcz.info>
2023-09-17 12:15:03 -0500

    Register all devices

--- src/xenia/emulator.cc
+++ src/xenia/emulator.cc
@@ -891,14 +891,15 @@ X_STATUS Emulator::CompleteLaunch(const std::filesystem::path& path,
   // Partition1 will go to this. Registering during CompleteLaunch allows us
   // to make sure any HostPathDevices are ready beforehand. (see comment above
   // cache:\ device registration for more info about why)
-  auto null_paths = {std::string("\\Partition0"), std::string("\\Cache0"),
+  auto null_paths = {std::string("\\partition0"), std::string("\\Cache0"),
                      std::string("\\Cache1")};
   auto null_device =
       std::make_unique<vfs::HostPathDevice>("\\Device\\Harddisk0", null_paths, false);
   if (null_device->Initialize()) {
     file_system_->RegisterDevice(std::move(null_device));
-    file_system_->RegisterSymbolicLink("hdd0:", "\\Device\\Harddisk0\\Partition0")
+    file_system_->RegisterSymbolicLink("hdd0:", "\\Device\\Harddisk0\\partition0")
     file_system_->RegisterSymbolicLink("hdd0:", "\\Device\\Harddisk0\\Cache0")
+    file_system_->RegisterSymbolicLink("hdd0:", "\\Device\\Harddisk0\\Cache1")
   }
 
   // Reset state.
